f2c226b2e4b76a45e58e7839e40ad4fd2b0148cd
Felipe <fefelbf@gmail.com>
2024-08-20 00:18:53 -0300

    commit

--- banco de dados/bancolocal.js
+++ banco de dados/bancolocal.js
@@ -10,18 +10,21 @@ const userInformations = {
          owner: 'felip',
          content: 'hello world',
       }
-   ]
+   ],
+
+   //create
+   createPost(dados){
+      //cria posts na mmemóeias
+      userInformations.posts.push({
+         id: userInformations.posts.length + 1,
+         owner: dados.owner,
+         content: dados.content
+      });
+   }
 }
 
-//create
-function creatPost(dados){
-   userInformations.posts.push({
-      id: userInformations.posts.length + 1,
-      owner: dados.owner,
-      content: dados.content
-   });
-} creatPost({owner: 'felip', content: 'segundo tweet'});
-  creatPost({owner: 'felip', content: 'terceiroi tweet'});
+userInformations.createPost({ owner: 'felipe', content: 'segundo tweet' });
+console.log(userInformations);
 
 //read
 function pegaPosts(){
@@ -32,12 +35,10 @@ function pegaPosts(){
 function updatePostContent(id, newContent){
    const postUptaded = pegaPosts().find(() =>{
       return postMessage.id === id;
-   })
-   console.log(postUptaded);
+   });
    postUptaded.content = newContent;
 }
 updatePostContent('novo conteudo do tweet');
-console.log(pegaPosts());
 
 //Delete
 function deletePost(id){
